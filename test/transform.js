d50091ddd2c3fdd154bf58802d459ab88f34684e
Ondřej Machek <ondrej.machek@klikni.cz>
2016-01-16 21:47:00 +0100

    exceptions to the rules added

--- test/transform.js
+++ test/transform.js
@@ -4,6 +4,8 @@ var obj = {
 	message: "Hello",
 	username: "John Doe",
 	user_id: 1,
+	is_retweeted: false,
+	retweeted_status_id: null,
 	phone_country_code: "+420",
 	phone_number: "123 456 789",
 	swag: true
@@ -19,16 +21,21 @@ var rules = {
 		},
 		wot: function (user_id, swag) { return user_id && swag ? "such swag" : ":(" }
 	},
+	retweeted_status: {
+		id: "retweeted_status_id"
+	},
 	phone1: "phone_number",
 	phone2: "phone_number",
 	phone3: "phone_number",
 	phone4: "phone_number"
 }
 
+var blacklist_exceptions = [ function (is_retweeted) { return !is_retweeted ? "retweeted_status" : null } ]
+
 console.log('\033[0;31mFiltered attributes\033[0m');
-var transformed = flat.transform.filter(rules, obj)
+var transformed = flat.transform.filter(rules, blacklist_exceptions, obj)
 console.log(JSON.stringify(transformed, null, 2))
 
 console.log('\n\033[0;31mPreserved attributes\033[0m');
-var transformed = flat.transform.preserve(rules, obj)
+var transformed = flat.transform.preserve(rules, blacklist_exceptions, obj)
 console.log(JSON.stringify(transformed, null, 2).replace(/("swag": true)/, "\033[0;32m$1\033[0m").replace(/("message": "Hello")/, "\033[0;32m$1\033[0m"))
